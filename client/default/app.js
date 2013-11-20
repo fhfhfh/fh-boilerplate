657e92401a9c04aca82087a8e9754feaf384e3b9
fhfhfh <fintan.mahon@feedhenry.com>
2013-11-20 09:59:08 +0000

    try webview

--- client/default/app.js
+++ client/default/app.js
@@ -25,13 +25,29 @@ function uiInit(pagesArray){
 
 	// TODO: move this to Login View file
 	$('#start').unbind().bind('click', function(){
-		LoginCtr.login('user', 'pass', function(success, msg){
-			if(success){
-				alert("Login Successful");
-			}else{
-				alert("Login Failed: " + msg);
-			}
-		});
+		var link = "https://www.google.ie";
+             $fh.webview({
+              'act': 'open',
+              'url': link,
+              'title': 'Addleshaw Goddard',
+              'x': '20px',
+              'y': '20px',
+              'width': '900px',
+              'height': '700px',
+              'showTitleBar': 'true',
+              'showTitleControls': 'true'
+            }, function(res) {
+              if (res === "opened") {
+                //webview window is now open
+                console.log("webview opened");
+              }
+              if (res === "closed") {
+                //webview window is now closed
+                console.log("webview closed");
+              }
+            }, function(msg, err) {
+              console.log('error from webview ',msg,err);
+            });
 	});
 
 	//TODO: Decide on best practice for init UI, without prescribing
